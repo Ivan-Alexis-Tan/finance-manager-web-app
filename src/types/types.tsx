79968c4a61655373c25b912114992666b7914ac8
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-11 23:04:11 +0800

    refactor (types): transactions table related types

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,20 +1,14 @@
-import type { transactionsCreateInput } from "../generated/prisma/models";
+import { Transactions } from "../generated/prisma/client";
+import type { TransactionsCreateInput } from "../generated/prisma/models";
 import { amountFormat, transactionMode, transactions } from "../helpers/constants";
 
 export interface TransactionsActionState { 
-    errors?: {
-        date?: string[]
-        details?: string[]
-        amount?: string[]
-        transaction?: string[]
-        transaction_mode?: string[]
-        category?: string[]
-    };
+    errors?: Partial<Record<keyof TransactionsType, string[] | undefined>>;
     message?: string | null; 
 }
 export type TransactionErrorsStates = TransactionsActionState['errors']
 
-export type TransactionColumnNames = keyof transactionsCreateInput
+export type TransactionColumnNames = keyof TransactionsCreateInput
 export type TransactionColumn = typeof transactions[number]
 export type TransactionMode = typeof transactionMode[number]
 
@@ -25,6 +19,13 @@ export interface FormErrorMsgerType {
     describedBy: string
     styles: string
     colName: keyof NonNullable<TransactionsActionState['errors']>
+}
+
+export type TransactionsType = Omit<Transactions, "total" | "trans_no" | "transaction_mode" | "transaction"> & { 
+    trans_no?: number
+    total: number 
+    transaction: TransactionColumn
+    transaction_mode: TransactionMode
 }
 
 export interface Transaction {
