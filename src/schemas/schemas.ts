1e163e686820bafb290b28036d9eb47f2b4f9644
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-12 00:08:31 +0800

    refactor (schema): adapts with new transactions table columns

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -7,28 +7,45 @@ export const schemaFormAction = z.object({
 })
 
 export const schemaTransactions = z.object({
-    id: z.coerce.number(),
+    trans_no: z.coerce.number().nullable(),
+
     date: z.date(
         { error: "Date is required."},
     ),
+
     details: z.string().min(1,
         { error: "Must have description of the transaction." }
     ),
+
+    quantity: z.coerce.number().min(1,
+        { error: "Must be equal or greater than 1." }
+    ),
+
     amount: z.coerce.number(
         { error: "Must have transaction amount" }
-    )
-        .min(0, { error: "Amount must be equal or greater than 0." }),
+    ).min( 0, 
+        { error: "Amount must be equal or greater than 0." }
+    ),
+
+    total: z.coerce.number().min(0, 
+        { error: "Must be greater or equal to 1" }
+    ),
+
     transaction: z.enum(transactions,
         { error: "Please select a transaction." }
     ),
+
     transaction_mode: z.enum(transactionMode,
         { error: "Please select a mode of transaction." }
     ),
+
     category: z.string({
         error: "Category cannot be null."
     }).min(1, {
         error: "Please select a category."
-    })
+    }),
+
+    userId: z.coerce.number().min(0).nullable(),
 })
 
-export const schemaTransactionsFormData = schemaTransactions.omit({ id: true })
+export const schemaTransactionsFormData = schemaTransactions.omit({ trans_no: true, userId: true })
