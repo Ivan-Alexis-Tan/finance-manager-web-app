b2ee2ea6acc4f247245a8017596ad9017487bf07
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-04 01:48:35 +0800

    feat (login): add login UI accessibility

--- src/app/Sign-in.tsx
+++ src/app/Sign-in.tsx
@@ -2,28 +2,58 @@
 
 import Image from "next/image"
 import { SigninWithGithub, SigninWithGoogle } from "../actions/auth"
+import { useState } from "react"
+import { capsEveryWord } from "../helpers/helperFn"
 
 export default function Signin() {
+    const [loading, setLoading] = useState(false)
+    const [method, setMethod] = useState("")
+
+    const signInMethods = {
+        google: SigninWithGoogle,
+        github: SigninWithGithub,
+    }
+
+    async function SignIn(method: keyof (typeof signInMethods)) {
+        setLoading(true)
+        setMethod(method)
+        
+        await signInMethods[method]()
+    }
+
+    function cancelSignin() {
+        setLoading(false)
+        setMethod("")
+    }
+
     return (
-        <div className="flex flex-wrap justify-center items-center gap-5 p-5 bg-(--fg-tint-clr) rounded-2xl"
+        <div className="p-5 bg-(--fg-tint-clr) rounded-2xl
+                        [&_button]:gap-3 [&_button]:p-2 [&_button]:flex [&_button]:flex-wrap
+                        [&_button]:justify-center [&_button]:items-center [&_button]:hover:bg-(--accent-clr)
+                        [&_button]:rounded-xl [&_button]:bg-foreground [&_button]:text-background"
         >
-            <h2 className="text-2xl font-bold">Continue with:</h2>
+            {loading
+                ? <div className="flex justify-evenly items-center">
+                    <h2 className="text-2xl font-bold">Signing in using {capsEveryWord(method)}...</h2>
+                    <button onClick={cancelSignin}>Cancel</button>
+                </div>
+                : <div className="flex flex-wrap justify-center items-center gap-5">
+                    <h2 className="text-2xl font-bold">Continue with:</h2>
 
-            <div className="flex flex-wrap justify-center gap-5
-                            [&>button]:flex [&>button]:flex-wrap [&>button]:justify-center [&>button]:gap-3 [&>button]:items-center [&>button]:p-2
-                            [&>button]:hover:bg-(--accent-clr)
-                            [&>button]:rounded-xl [&>button]:bg-foreground [&>button]:text-background"
-            >
-                <button onClick={_ => SigninWithGoogle()}>
-                    <Image src={"/google-logo.png"} alt="google-logo" width={30} height={30} />
-                    <span>Google</span>
-                </button>
+                    <div className="flex flex-wrap justify-center gap-5"
+                    >
+                        <button onClick={_ => SignIn("google")} disabled={loading}>
+                            <Image src={"/google-logo.png"} alt="google-logo" width={30} height={30} />
+                            <span>Google</span>
+                        </button>
 
-                <button onClick={_ => SigninWithGithub()}>
-                    <Image src="/github-logo.png" alt="github-logo" width={30} height={30} />
-                    <span>GitHub</span>
-                </button>
-            </div>
+                        <button onClick={_ => SignIn("github")} disabled={loading}>
+                            <Image src="/github-logo.png" alt="github-logo" width={30} height={30} />
+                            <span>GitHub</span>
+                        </button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
