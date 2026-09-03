8da5bf94dd959b7d04cee7a593fcc35c57f80fa9
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-04 00:25:53 +0800

    refactor (records): style and dom of transaction table

--- src/app/records/TransactionsTable.tsx
+++ src/app/records/TransactionsTable.tsx
@@ -8,9 +8,12 @@ import { capsEveryWord, dateFormatter } from "@/src/helpers/helperFn";
 import { deleteTransaction } from "@/src/actions/actions";
 import { PopupWindow, usePopupWindow } from "@/src/components/PopupWindows";
 
-type TransactionTable = { transactionData: TransactionsType[] }
+type TransactionTable = { 
+    transactionData: TransactionsType[] 
+    className?: string
+}
 
-export default function TransactionsTable({ transactionData = [] }: TransactionTable ) {
+export default function TransactionsTable({ transactionData = [], className }: TransactionTable ) {
     const [showPopup, setShowPopup] = useState<Boolean>(false)
 
     const { popupStates, setPopupWindow } = usePopupWindow({
@@ -23,65 +26,62 @@ export default function TransactionsTable({ transactionData = [] }: TransactionT
 
     return (
         transactionData.length >= 1
-            ? <div className="flex justify-center mb-5">
-
+            ? <div className={`${className ?? ""} border-collapse`}>
                 {showPopup
                     && <PopupWindow popupStates={popupStates} />
                 }
-
-                <div className="max-w-300 overflow-x-auto px-5 pb-2 bg-gray-700 rounded border-collapse">
-                    <table className="w-full min-w-md [&_td]:p-1">
-                        <thead>
-                            <tr className="border-b border-b-gray-900 font-bold">
-                                <td></td>
-                                <td>Date</td>
-                                <td>Details</td>
-                                <td>Quantity</td>
-                                <td>Amount</td>
-                                <td>Total</td>
-                                <td>Transaction</td>
-                                <td>Transaction Mode</td>
-                                <td>Category</td>
+                
+                <table className="[&_td]:p-1 w-full bg-gray-700">
+                    <thead>
+                        <tr className="border-b border-b-gray-900 font-bold">
+                            <td></td>
+                            <td>Date</td>
+                            <td>Details</td>
+                            <td>Quantity</td>
+                            <td>Amount</td>
+                            <td>Total</td>
+                            <td>Transaction</td>
+                            <td>Transaction Mode</td>
+                            <td>Category</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {transactionData.map(data => (
+                            <tr key={data.trans_no}>
+                                <td>
+                                    <div className="flex gap-1">
+                                        <Link title="Edit"
+                                            href={`/records/${data.trans_no}/edit`}
+                                        >
+                                            ✏️
+                                        </Link>
+                                        <button title="Delete"
+                                            onClick={_ => {
+                                                setShowPopup(true)
+                                                setPopupWindow(p => ({
+                                                    ...p,
+                                                    details: <>You trying to delete "<i>{data.details}</i>"</>,
+                                                    onConfirm() {
+                                                        deleteTransaction(data.trans_no as number)
+                                                        setShowPopup(false)
+                                                    }
+                                                }))
+                                            }}
+                                        >🗑️</button>
+                                    </div>
+                                </td>
+                                <td>{dateFormatter(`${data.date}`)}</td>
+                                <td>{data.details}</td>
+                                <td className="text-center">{data.quantity}</td>
+                                <td>₱{(data.amount)?.toLocaleString()}</td>
+                                <td>₱{Number(data.total)}</td>
+                                <td>{capsEveryWord(data.transaction)}</td>
+                                <td>{capsEveryWord(data.transaction_mode)}</td>
+                                <td>{capsEveryWord(data.category)}</td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {transactionData.map(data => (
-                                <tr key={data.trans_no}>
-                                    <td>
-                                        <div className="flex gap-1">
-                                            <Link title="Edit"
-                                                href={`/records/${data.trans_no}/edit`}
-                                            >
-                                                ✏️
-                                            </Link>
-                                            <button title="Delete"
-                                                onClick={_ => {
-                                                    setShowPopup(true)
-                                                    setPopupWindow(p => ({
-                                                        ...p,
-                                                        details: <>You trying to delete "<i>{data.details}</i>"</>,
-                                                        onConfirm() {
-                                                            deleteTransaction(data.trans_no as number)
-                                                            setShowPopup(false)
-                                                        }
-                                                    }))
-                                                }}
-                                            >🗑️</button>
-                                        </div>
-                                    </td>
-                                    <td>{dateFormatter(`${data.date}`)}</td>
-                                    <td>{data.details}</td>
-                                    <td className="text-center">{data.quantity}</td>
-                                    <td>₱{(data.amount)?.toLocaleString()}</td>
-                                    <td>₱{Number(data.total)}</td>
-                                    <td>{capsEveryWord(data.transaction)}</td>
-                                    <td>{capsEveryWord(data.transaction_mode)}</td>
-                                    <td>{capsEveryWord(data.category)}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                        ))}
+                    </tbody>
+                </table>
             </div>
             : <div>No transaction to show</div>
     )
