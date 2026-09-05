0c8e4fc06a0d0e3dbb2365baac308c282f371c1e
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-05 15:23:07 +0800

    feat (login): add loading animation

--- src/app/Sign-in.tsx
+++ src/app/Sign-in.tsx
@@ -1,9 +1,12 @@
 "use client"
 
 import Image from "next/image"
-import { SigninWithGithub, SigninWithGoogle } from "../actions/auth"
 import { useState } from "react"
+
+import { SigninWithGithub, SigninWithGoogle } from "../actions/auth"
 import { capsEveryWord } from "../helpers/helperFn"
+
+import Spinner from "../components/Spinner"
 
 export default function Signin() {
     const [loading, setLoading] = useState(false)
@@ -27,15 +30,23 @@ export default function Signin() {
     }
 
     return (
-        <div className="p-5 bg-(--fg-tint-clr) rounded-2xl
+        <div className={`p-5 bg-(--fg-tint-clr) rounded-2xl
                         [&_button]:gap-3 [&_button]:p-2 [&_button]:flex [&_button]:flex-wrap
-                        [&_button]:justify-center [&_button]:items-center [&_button]:hover:bg-(--accent-clr)
-                        [&_button]:rounded-xl [&_button]:bg-foreground [&_button]:text-background"
+                        [&_button]:justify-center [&_button]:items-center
+                        [&_button]:rounded-xl [&_button]:text-background
+                        [&_button]:bg-foreground [&_button]:hover:bg-(--accent-clr)`}
         >
             {loading
-                ? <div className="flex justify-evenly items-center">
-                    <h2 className="text-2xl font-bold">Signing in using {capsEveryWord(method)}...</h2>
-                    <button onClick={cancelSignin}>Cancel</button>
+                ? <div className="gap-3 flex flex-wrap justify-evenly items-center">
+                    <div className="gap-3 flex flex-wrap items-center">
+                        <Spinner className="mx-auto" />
+                        <h2 className="text-center text-2xl font-bold">Signing in using {capsEveryWord(method)}...</h2>
+                    </div>
+                    <button onClick={cancelSignin}
+                        className="bg-foreground hover:bg-(--red-clr)! hover:text-foreground"
+                    >
+                        Cancel
+                    </button>
                 </div>
                 : <div className="flex flex-wrap justify-center items-center gap-5">
                     <h2 className="text-2xl font-bold">Continue with:</h2>
