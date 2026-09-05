b2afdece20597361765774013856e7f58b4fbca0
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-05 14:58:06 +0800

    feat (records): add loading accessibility on create transaction form

--- src/app/records/create/page.tsx
+++ src/app/records/create/page.tsx
@@ -1,11 +1,13 @@
 "use server"
 
 import { Session } from "next-auth";
+import { Suspense } from "react";
 
 import { getCategories, getDistinctDetails } from "@/src/actions/actions"
 import { auth } from "@/auth"
 
 import CreateTransactionForm from "./CreateTransactionForm"
+import Loading from "@/src/components/Loading";
 
 export default async function Page() {
     const session = await auth();
@@ -16,11 +18,13 @@ export default async function Page() {
         <div className="flex flex-col border-2 rounded-2xl m-6 md:m-10 md:p-8 p-4">
             <h1 className="text-4xl text-center font-bold">Create Transaction</h1>
 
-            <CreateTransactionForm 
-                categories={categories}
-                distinctDetails={distinctDetails}
-                user={session?.user as Session["user"]} 
-            />
+            <Suspense fallback={<Loading className="my-20" />}>
+                <CreateTransactionForm 
+                    categories={categories}
+                    distinctDetails={distinctDetails}
+                    user={session?.user as Session["user"]} 
+                />
+            </Suspense>
         </div>
     )
 }
