fb3c603f3854e1dd483dc240eefcbef5305b4149
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-05 17:21:17 +0800

    feat (nav): add logout spinner

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
 import { Session } from "next-auth";
-import { useState } from "react";
 
 import { logout } from "../actions/auth";
 import { PopupWindow, usePopupWindow } from "../components/PopupWindows";
+
+import Spinner from "../components/Spinner";
 
 interface NavBar {
     session: Session
@@ -13,14 +16,20 @@ interface NavBar {
 
 export default function NavBar({ session }: NavBar) {
     const [logoutWindow, setLogoutWindow] = useState(false)
+    const [loadLogout, setLoadLogout] = useState(false)
+
+    useEffect(() => {
+        if (!session?.user?.id) setLoadLogout(false)
+    }, [session?.user?.id])
 
     const { popupStates } = usePopupWindow({
         header: "Confirm Logout",
         details: `Logout ${session?.user?.name ?? "this account"}?`,
         confirmText: "Logout",
         onConfirm() {
-            logout()
             setLogoutWindow(false)
+            setLoadLogout(true)
+            logout()
         },
         onCancel() {
             setLogoutWindow(false)
@@ -33,6 +42,15 @@ export default function NavBar({ session }: NavBar) {
             
             {logoutWindow
                 && <PopupWindow popupStates={popupStates} />
+            }
+
+            {loadLogout
+                && <div className="w-full h-full fixed top-0 left-0 flex justify-center items-center bg-transparent">
+                    <div className="w-50 h-50 sm:w-100 sm:h-100 gap-5 flex flex-col justify-center items-center rounded-2xl bg-foreground border border-white">
+                        <Spinner className="" />
+                        <h3 className="text-xl font-bold text-background">Logging out...</h3>
+                    </div>
+                </div>
             }
 
             <div>
